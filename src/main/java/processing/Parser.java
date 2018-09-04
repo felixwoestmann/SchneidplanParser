5ff3ec09717eeb4f482bb5a2b00d5163ba60f687
Felix Wöstmann <f.wosti@gmail.com>
2018-09-04 09:30:04 +0200

    Added Tests

--- src/main/java/processing/Parser.java
+++ src/main/java/processing/Parser.java
@@ -1,6 +1,7 @@
 package processing;
 
 import model.Einzelteil;
+import model.Parsable;
 import model.Schneidplan;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -10,34 +11,79 @@ import org.jsoup.select.Elements;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.function.Consumer;
 
 public class Parser {
     private final String ENCODING = "UTF-8";
 
+    /**
+     * Parse the Information out of the Einzelteile section of the table!
+     * @param einzelteile
+     * @return
+     */
     private ArrayList<Einzelteil> parseEinzelteile(Element einzelteile) {
+        ArrayList<Einzelteil> einzelteilList=new ArrayList<>();
+        /*
+           We have to discriminate between different Einzeltiele in the table.
+           We do that by assuming, that a new Einzelteil starts when there are three table cells in a table row.
+           We collect all rows in the subtable until a new row with three cells start.
+         */
+        ArrayList<Element> subtable=new ArrayList<>();
+        Elements tablerows =  einzelteile.select("tr");
 
+        for(Element tablerow:tablerows) {
+            //does new table of einzelteile start ?
+            if(tablerow.childNodeSize()==3){
+                //new table, but list is empty? just add elements
+                if(subtable.isEmpty()){
+                    subtable.add(tablerow);
+                }else{
+                //new table, but list is filled? Parse list and empty list
+                    einzelteilList.add(parseEinzelteil(subtable));
+                    subtable=new ArrayList<>();
+                }
+            }else {
+                subtable.add(tablerow);
+            }
+        }
+        return einzelteilList;
+    }
+
+    private Einzelteil parseEinzelteil(ArrayList<Element> tablerows) {
+        Einzelteil einzelteil=new Einzelteil();
+        tablerows.parallelStream().forEach(element -> processTableRow(einzelteil,element));
+        //TODO parralelstream thread safe?
         return null;
     }
 
-    private Einzelteil parseEinzelteil() {
-        return null;
+    /**
+     * Parses a tablerow and calls setAttribute on the given parsable
+     * @param parsable
+     * @param tablerow
+     */
+    private void processTableRow(Parsable parsable,Element tablerow) {
+        Elements cells = tablerow.select("td");
+        //only add if the row got at least two cells
+        if (cells.size() >= 2) {
+            String trigger = cells.get(0).text();
+            String content = cells.get(1).text();
+            trigger = removeWeirdWhitespace(trigger);
+            parsable.setAttribute(trigger, content);
+        }
     }
 
+    /**
+     * Parse the information out of the header of a Schneidplan.
+     * @param headertable
+     * @return
+     */
     private Schneidplan parseSchneideplanHead(Element headertable) {
         Schneidplan schneidplan = new Schneidplan();
         //get all tablerows
         Elements tablerows = headertable.select("tr");
         //get all table cells of row
         for (Element tablerow : tablerows) {
-            Elements cells = tablerow.select("td");
-            //only add if the row got at least two cells
-            if (cells.size() >= 2) {
-                String trigger = cells.get(0).text();
-                String content = cells.get(1).text();
-                trigger = removeWeirdWhitespace(trigger);
-                schneidplan.setAttribute(trigger, content);
-            }
-
+         processTableRow(schneidplan,tablerow);
         }
         return schneidplan;
 
@@ -71,7 +117,7 @@ public class Parser {
         Element einzelteile = tables.get(3);
 
         Schneidplan schneideplan = parseSchneideplanHead(head);
-        schneideplan.setEinzelteile(parseEinzelteile(Element einzelteile));
+      //  schneideplan.setEinzelteile(parseEinzelteile(Element einzelteile));
         return schneideplan;
     }
 }
--- src/test/java/TestParser.java
+++ src/test/java/TestParser.java
@@ -1,12 +1,89 @@
+import model.Einzelteil;
+import model.Parsable;
+import model.Schneidplan;
 import org.junit.Test;
 import processing.Parser;
 
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+
+import static junit.framework.Assert.assertNotNull;
+import static junit.framework.TestCase.assertTrue;
+
 public class TestParser {
 
-@Test
-public void testParseSchneidplan() {
-Parser parser=new Parser();
-parser.parseSchneidplan("Reference/einrichteplan_1.htm");
+    @Test
+    public void testParseSchneidplan() throws IllegalAccessException {
+        Parser parser = new Parser();
+        Schneidplan schneidplan = parser.parseSchneidplan("Reference/einrichteplan_1.htm");
 
-}
+    }
+
+
+    /**
+     * Test if all fields of Schneidplan are filled.
+     */
+    @Test
+    public void testSchneidplanFilledWithHTML_1() {
+        Parser parser = new Parser();
+        Schneidplan schneidplan = parser.parseSchneidplan("Reference/einrichteplan_1.htm");
+        checkFilledSchneidplan(schneidplan);
+    }
+
+    /**
+     * Test if all fields of Schneidplan are filled.
+     */
+    @Test
+    public void testSchneidplanFilledWithHTML_2() {
+        Parser parser = new Parser();
+        Schneidplan schneidplan = parser.parseSchneidplan("Reference/einrichteplan_2.htm");
+        checkFilledSchneidplan(schneidplan);
+    }
+
+    @Test
+    public void testEinzelteileFilledWithHTML_1() {
+        Parser parser = new Parser();
+        Schneidplan schneidplan = parser.parseSchneidplan("Reference/einrichteplan_1.htm");
+        ArrayList<Einzelteil> einzelteile = schneidplan.getEinzelteile();
+        checkFilledEinzelteil(einzelteile);
+    }
+
+    @Test
+    public void testEinzelteileFilledWithHTML_2() {
+        Parser parser = new Parser();
+        Schneidplan schneidplan = parser.parseSchneidplan("Reference/einrichteplan_2.htm");
+        ArrayList<Einzelteil> einzelteile = schneidplan.getEinzelteile();
+        checkFilledEinzelteil(einzelteile);
+    }
+
+    private void checkFilledEinzelteil(ArrayList<Einzelteil> einzelteile) {
+        for (Einzelteil teil : einzelteile) {
+            assertNotNull(teil.getAbmessungen());
+            assertNotNull(teil.getAnazhl());
+            assertNotNull(teil.getAnzahl_einstechpunkte());
+            assertNotNull(teil.getBearbeitungszeit());
+            assertNotNull(teil.getEinstechzeit());
+            assertNotNull(teil.getFlaeche());
+            assertNotNull(teil.getGeofilename());
+            assertNotNull(teil.getGewicht());
+            assertNotNull(teil.getSchneidlaenge());
+
+        }
+    }
+
+    private void checkFilledSchneidplan(Schneidplan schneidplan) {
+        assertNotNull(schneidplan.getAnzahl_programmdurchlaeufe());
+        assertNotNull(schneidplan.getGesamtschnittlaenge());
+        assertNotNull(schneidplan.getGewicht());
+        assertNotNull(schneidplan.getMaschinenzeit());
+        assertNotNull(schneidplan.getMaterial_id());
+        assertNotNull(schneidplan.getMindestzuschnitt());
+        assertNotNull(schneidplan.getProgrammname());
+        assertNotNull(schneidplan.getVerschnitt());
+        assertNotNull(schneidplan.getZuschnitt());
+
+
+    }
+
+
 }
