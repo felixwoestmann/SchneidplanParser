ba49f4ee0b255c2ea02e5fdb7486f07cf5581de5
Felix Wöstmann <f.wosti@gmail.com>
2018-09-11 16:50:18 +0200

    Updated CSVProcessor

--- src/main/java/processing/CSVProcessor.java
+++ src/main/java/processing/CSVProcessor.java
@@ -11,41 +11,45 @@ import model.Schneidplan;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.function.Consumer;
 
 public class CSVProcessor {
+    private final String SEPERATOR = ",";
 
     public String writeToString(Schneidplan schneidplan) {
-        StringWriter writer=new StringWriter();
-        write(schneidplan,writer);
+        StringWriter writer = new StringWriter();
+        write(schneidplan, writer);
         return writer.toString();
     }
 
     public void writeToFile(Schneidplan schneidplan, String path) {
-        FileWriter writer= null;
+        FileWriter writer = null;
         try {
             writer = new FileWriter(path);
         } catch (IOException e) {
             System.out.println("Etwas lief schief!");
             e.printStackTrace();
         }
-        write(schneidplan,writer);
+        write(schneidplan, writer);
     }
 
-    private void write(Schneidplan schneidplan, Writer writer) {
 
 
-        StatefulBeanToCsv beanToCsv = new StatefulBeanToCsvBuilder(writer)
-                .withQuotechar(CSVWriter.NO_QUOTE_CHARACTER)
-                .build();
 
-
+    private void write(Schneidplan schneidplan, Writer writer)  {
+        StringBuilder sb = new StringBuilder();
+        //addheadings
+        appendLine(sb, "Beschreibung:", "Wert:");
+        schneidplan.toCSV().forEach(strings -> appendLine(sb, strings[0], strings[1]));
         try {
-            beanToCsv.write(schneidplan.getEinzelteile());
-        } catch (CsvDataTypeMismatchException | CsvRequiredFieldEmptyException e) {
-            System.out.println("Etwas lief schief");
+            writer.write(sb.toString());
+        } catch (IOException e) {
             e.printStackTrace();
         }
+    }
 
-
+    private void appendLine(StringBuilder sb, String leftcolumn, String rightcolumn) {
+        sb.append(leftcolumn).append(SEPERATOR).append(rightcolumn).append("\n");
     }
 }
