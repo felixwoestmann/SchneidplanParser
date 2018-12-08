db40ca778a7139f2887e894cb56d7acedd200b22
felixwoestmann <git@lostincoding.de>
2018-12-08 14:55:53 +0100

    Implemented further UI adjustments

--- src/main/java/ui/SchneidplanGUI.java
+++ src/main/java/ui/SchneidplanGUI.java
@@ -6,9 +6,8 @@ import javafx.collections.ObservableList;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.ProgressBar;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.DragEvent;
 import javafx.scene.input.Dragboard;
@@ -18,8 +17,13 @@ import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 import model.Schneidplan;
+import org.apache.commons.compress.compressors.FileNameUtil;
+import org.apache.commons.io.FilenameUtils;
 import processing.Parser;
+import processing.Processor;
+import processing.xlxs.XLXSAppender;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 
@@ -29,14 +33,25 @@ public class SchneidplanGUI extends Application {
     //
     private MenuItem about;
     private MenuItem showLog;
-    //
-    private ImageView imageview;
+    // step_one
+    private ImageView dropzone_one;
+    private TextArea text_one;
+    // step_two
+    private DatePicker datePicker_two;
+    private TextArea text_two;
+    // step_three
+    private ImageView dropzone_three;
+    private TextArea text_three;
     //
     private ProgressBar progressBar;
     //
     private Parser parser;
+    private Processor processor;
     private Schneidplan schneidplan;
     private LogWindow logWindow = null;
+    private File excelFile = null;
+    private Image successImage;
+    private Image plusImage;
 
     public static void main(String[] args) {
         launch(args);
@@ -53,6 +68,7 @@ public class SchneidplanGUI extends Application {
         }
         obtainUiElements();
         parser = new Parser();
+        processor = new XLXSAppender();
         setUpFunctionality();
         //set action which should be performed if windows is closed
         stage.getScene().getWindow().addEventFilter(WindowEvent.WINDOW_CLOSE_REQUEST, this::close);
@@ -76,6 +92,8 @@ public class SchneidplanGUI extends Application {
         stage.setScene(scene);
         stage.show();
         stage.setTitle("Schneidplan Parser");
+        successImage = new Image("images/success.png");
+        plusImage = new Image("images/plus.png");
     }
 
 
@@ -86,16 +104,26 @@ public class SchneidplanGUI extends Application {
         ObservableList<MenuItem> menuItems = bar.getMenus().get(0).getItems();
         about = menuItems.get(0);
         showLog = menuItems.get(1);
+        //misc
         progressBar = (ProgressBar) sc.lookup("#progress");
-        imageview = (ImageView) sc.lookup("#imageview");
+        //step_one
+        dropzone_one = (ImageView) sc.lookup("#dropzone_one");
+        text_one = (TextArea) sc.lookup("#text_one");
+        //step_two
+        datePicker_two = (DatePicker) sc.lookup("#datepicker_two");
+        text_two = (TextArea) sc.lookup("#text_two");
+        //step_one
+        dropzone_three = (ImageView) sc.lookup("#dropzone_three");
+        text_three = (TextArea) sc.lookup("#text_three");
     }
 
     private void setUpFunctionality() {
         showLog.setOnAction(actionEvent -> openLogWindow());
-        imageview.setOnDragOver(new EventHandler<DragEvent>() {
+        //step_one
+        dropzone_one.setOnDragOver(new EventHandler<DragEvent>() {
             @Override
             public void handle(DragEvent event) {
-                if (event.getGestureSource() != imageview
+                if (event.getGestureSource() != dropzone_one
                         && event.getDragboard().hasFiles()) {
                     /* allow for both copying and moving, whatever user chooses */
                     event.acceptTransferModes(TransferMode.COPY_OR_MOVE);
@@ -103,19 +131,29 @@ public class SchneidplanGUI extends Application {
                 event.consume();
             }
         });
-        imageview.setOnDragDropped(new EventHandler<DragEvent>() {
+        dropzone_one.setOnDragDropped(new EventHandler<DragEvent>() {
             @Override
             public void handle(DragEvent event) {
                 Dragboard db = event.getDragboard();
                 boolean success = false;
+                //chek if drag contains files
                 if (db.hasFiles()) {
-                  CustomLogger.getInstance().log(db.getFiles().toString());
-                    success = true;
+                    //check if drag only contains one file
+                    if (db.getFiles().size() == 1) {
+                        excelFile = db.getFiles().get(0);
+                        //chek if the file is correct based on extension in awaiting processor
+                        if (FilenameUtils.getExtension(excelFile.getAbsolutePath()).equals(processor.getFileExtensionWithoutDot())) {
+                            CustomLogger.getInstance().log(db.getFiles().toString());
+                            success = true;
+                            dropzone_one.setImage(successImage);
+                        }
+                    }
+
+
                 }
                 /* let the source know whether the string was successfully
                  * transferred and used */
                 event.setDropCompleted(success);
-
                 event.consume();
             }
         });
